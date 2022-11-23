d7524325dd24b4989187e4d7738df6f2553c9cda
lauromiottojr <junior.miotto36@gmail.com>
2022-11-23 14:50:44 -0300

    Thymeleaf config at posts.html

--- src/main/java/com/blogspring/controllers/PostController.java
+++ src/main/java/com/blogspring/controllers/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,15 @@ public class PostController {
 	private PostService postService;
 
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
-	public ModelAndView getPost() {
+	public ModelAndView getPosts() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<PostModel> posts = postService.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+	}
+
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") Long id) {
 		ModelAndView mv = new ModelAndView("posts");
 		List<PostModel> posts = postService.findAll();
 		mv.addObject("posts", posts);
--- src/main/java/com/blogspring/utils/DummyData.java
+++ src/main/java/com/blogspring/utils/DummyData.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,26 +18,35 @@ public class DummyData {
 	@Autowired
 	private PostRepository postRepository;
 
-	// @PostConstruct
+	//@PostConstruct
 	public void savePosts() {
 		List<PostModel> postList = new ArrayList<>();
 
-		PostModel post1 = new PostModel();
+		/*
+		 * PostModel post1 = new PostModel();
+		 * 
+		 * post1.setAuthor("Lauro"); post1.setDateCriation(LocalDate.now());
+		 * post1.setTitle("Docker"); post1.setText("Inserting new docker post!");
+		 * 
+		 * PostModel post2 = new PostModel();
+		 * 
+		 * post2.setAuthor("Kaka"); post2.setDateCriation(LocalDate.now());
+		 * post2.setTitle("Java"); post2.setText("Inserting new java post!");
+		 * 
+		 * postList.add(post1); postList.add(post2);
+		 */
 
-		post1.setAuthor("Lauro");
-		post1.setDateCriation(LocalDate.now());
-		post1.setTitle("Docker");
-		post1.setText("Inserting new docker post!");
+		PostModel post3 = new PostModel();
 
-		PostModel post2 = new PostModel();
+		post3.setAuthor("Pedro A. Cabral");
+		post3.setDateCriation(LocalDate.now());
+		post3.setTitle("Carta para o Brasil");
+		post3.setText(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at venenatis augue. Nullam eleifend lacus lectus, vel mollis arcu molestie a. Etiam nunc orci, euismod a urna sit amet, elementum dictum dolor. Quisque congue scelerisque ultricies. Curabitur congue enim ligula, in sodales tellus pretium non. Curabitur in aliquam orci. Nam eget lacinia ligula. Fusce ac dignissim leo. Cras suscipit, justo non sodales condimentum, quam tortor lacinia nisl, et efficitur est orci at felis. Vivamus non dapibus turpis. Sed malesuada euismod massa, nec ornare mi eleifend ac. Nunc fringilla, massa ut viverra convallis, leo arcu commodo nulla, ut vehicula mauris tellus sagittis nunc. Proin sit amet ultrices mauris.\r\n"
+						+ "\r\n"
+						+ "Cras in finibus nibh, quis aliquet ligula. Aenean interdum convallis arcu, non consequat eros tincidunt sit amet. Sed est tortor, accumsan eget nibh a, rutrum ornare sem. Cras a viverra metus, vel bibendum nibh. Nulla imperdiet pretium fringilla. Praesent quis tristique nulla. Donec sagittis et augue eget laoreet. Praesent pulvinar commodo purus accumsan mattis. Donec ex tellus, hendrerit a turpis at, vulputate scelerisque urna. Nam ultricies nulla purus, at pretium elit convallis ut. Vestibulum sodales massa ut sagittis rutrum. Aliquam erat volutpat. Proin vestibulum convallis dolor, eu suscipit quam suscipit vitae. Nunc ac.");
 
-		post2.setAuthor("Kaka");
-		post2.setDateCriation(LocalDate.now());
-		post2.setTitle("Java");
-		post2.setText("Inserting new java post!");
-
-		postList.add(post1);
-		postList.add(post2);
+		postList.add(post3);
 
 		for (PostModel post : postList) {
 			PostModel postSaved = postRepository.save(post);
